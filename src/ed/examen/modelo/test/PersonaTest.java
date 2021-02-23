19861f6cce2ee45f19e9213236d92deb485ce04d
Adrián <dam011.2021@gmail.com>
2021-02-23 20:24:47 +0100

    testSetDni

--- src/ed/examen/modelo/test/PersonaTest.java
+++ src/ed/examen/modelo/test/PersonaTest.java
@@ -27,4 +27,30 @@ Persona p = new Persona("12345678W", "Adrián", "Mosquera");
 		
 	}
 	
+	@Test
+	void testSetDni() {
+		
+		//test valido
+		try {
+			p.setDni("12345678W");
+		} catch (Exception e) {
+		//	e.printStackTrace();
+		}
+		
+		assertEquals("12345678W", p.getDni());
+		
+		//test invalido
+		boolean b = false;
+		
+		try {
+			p.setDni("123456789012");
+		} catch (Exception e) {
+		//	e.printStackTrace();
+			b = true;
+		}
+		
+		assertTrue(b);
+		
+	}
+	
 }
